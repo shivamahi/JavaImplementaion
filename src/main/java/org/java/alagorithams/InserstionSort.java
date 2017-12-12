ffe0290a129401bfb6aee287539be213faa4a806
shivamahi <shivakumar.alapaka@gmail.com>
2017-12-13 01:10:53 +0530

    Implementing Logic
    Implementing  Sorting Logic like BubbleSort,Inserstion,SelectionSort

--- src/main/java/org/java/alagorithams/InserstionSort.java
+++ src/main/java/org/java/alagorithams/InserstionSort.java
@@ -1,9 +1,49 @@
 package org.java.alagorithams;
 
+import java.util.Scanner;
+
 public class InserstionSort {
+	private int n;
+
+	public void sort(int[] arr) {
+		n = arr.length;
+		for (int i = 1; i < n; i++) {
+			int j = i, index = arr[i];
+			while (j > 0 && arr[j - 1] > index) {
+				int temp = arr[j]; // or arr[j]=arr[j-1];
+				arr[j] = arr[j - 1]; // j--;
+				arr[j - 1] = temp; // }
+				j--; // a[j]=index
+			}
+		}
+
+	}
+
+	public void print(int[] arr) {
+		n = arr.length;
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+
+		}
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		InserstionSort bs = new InserstionSort();
+		int n;
+		int arr[];
+		System.out.println("Enter How many Numbers :");
+		n = new Scanner(System.in).nextInt();
+		System.out.println("Enter " + n + " numbers ");
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new Scanner(System.in).nextInt();
+		}
+		System.out.println("Before Sort Array : ");
+		bs.print(arr);
+		bs.sort(arr);
+		System.out.println("\nAfter Sort Array : ");
+		bs.print(arr);
 
 	}
 
--- src/main/java/org/java/alagorithams/SelectionSort.java
+++ src/main/java/org/java/alagorithams/SelectionSort.java
@@ -1,9 +1,52 @@
 package org.java.alagorithams;
 
+import java.util.Scanner;
+
 public class SelectionSort {
 
+	private int n;
+
+	public void sort(int[] arr) {
+		n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			int max = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[max] > arr[j]) {
+					max = j;
+				}
+			}
+			int temp = arr[max];
+			arr[max] = arr[i];
+			arr[i] = temp;
+
+		}
+
+	}
+
+	public void print(int[] arr) {
+		n = arr.length;
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n;
+		int arr[];
+		SelectionSort bs = new SelectionSort();
+		System.out.println("Enter How many Numbers :");
+		n = new Scanner(System.in).nextInt();
+		System.out.println("Enter " + n + " numbers ");
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new Scanner(System.in).nextInt();
+		}
+		System.out.println("Before Sort Array : ");
+		bs.print(arr);
+		bs.sort(arr);
+		System.out.println("\nAfter Sort Array : ");
+		bs.print(arr);
 
 	}
 
