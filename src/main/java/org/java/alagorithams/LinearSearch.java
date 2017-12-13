6a564c083eebe70ee316b30e970d572ad772b084
shivamahi <shivakumar.alapaka@gmail.com>
2017-12-13 23:26:49 +0530

    Linear Search Implimenation

--- src/main/java/org/java/alagorithams/LinearSearch.java
+++ src/main/java/org/java/alagorithams/LinearSearch.java
@@ -1,9 +1,37 @@
 package org.java.alagorithams;
 
+import java.util.Scanner;
+
 public class LinearSearch {
+	int n;
+
+	public int searchElement(int arr[], int ser) {
+		n = arr.length;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == ser)
+				return i;
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n, ser;
+		int arr[];
+		LinearSearch selectionSort = new LinearSearch();
+		System.out.println("Enter How many List Numbers :");
+		n = new Scanner(System.in).nextInt();
+		System.out.println("Enter " + n + " numbers ");
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new Scanner(System.in).nextInt();
+		}
+		System.out.println("\nEnter which element you want Search : ");
+		ser = new Scanner(System.in).nextInt();
+		int index = selectionSort.searchElement(arr, ser);
+		if (index == -1)
+			System.out.println("Element Not Found");
+		else
+			System.out.println("Elemnt Is Found In Position : " + index + 1);
 
 	}
 
